50a42fb057e2b5651a93310925bfc5932ae4ad80
jdzhang <673235694@qq.com>
2019-08-12 00:02:34 +0800

    solution PalindromeNumber

--- src/main/java/Array/PalindromeNumber.java
+++ src/main/java/Array/PalindromeNumber.java
@@ -13,11 +13,27 @@ package Array;
  */
 public class PalindromeNumber {
 
+    /**
+     * Description:
+     * Determine whether an integer is a palindrome. An integer is a palindrome when it reads the same backward as forward.
+     * Time Complexity:O(lg(n))    Space Complexity:O(1)
+     * @param x
+     * @return
+     */
     public boolean isPalindrome(int x){
-        
+        if(x<0 || (x%10==0 && x!=0)){
+            return  false;
+        }
+        int result=0;
+        while (x>result){
+            result=result*10+x%10;
+            x=x/10;
+        }
+        return x==result || x==result/10;
     }
 
     public static void main(String[] args) {
-
+        int x=121;
+        Boolean output= new PalindromeNumber().isPalindrome(x);
     }
 }
