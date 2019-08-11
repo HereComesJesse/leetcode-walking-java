33a3e0d7c09c7a5c6704bf1a3e01eafe86e3c88f
jdzhang <673235694@qq.com>
2019-08-11 23:50:05 +0800

    initial PalindromeNumber

---
+++ src/main/java/Array/PalindromeNumber.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright 2019 tuhu.cn All right reserved. This software is the
+ * confidential and proprietary information of tuhu.cn ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Tuhu.cn
+ */
+package Array;
+
+/**
+ * @author zhangjundong
+ * @date 2019/8/1123:45
+ */
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x){
+        
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
