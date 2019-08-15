9e894a82443173cb16ef1e002340a20c7bef779d
jdzhang <673235694@qq.com>
2019-08-15 23:50:48 +0800

    add mergeTwoLists

---
+++ src/main/java/Array/MergeTwoSortedLists.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2019 tuhu.cn All right reserved. This software is the
+ * confidential and proprietary information of tuhu.cn ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Tuhu.cn
+ */
+package Array;
+
+/**
+ * @author zhangjundong
+ * @date 2019/8/1523:48
+ */
+public class MergeTwoSortedLists {
+    public class ListNode{
+        int val;
+        ListNode next;
+        ListNode(int x){val=x;}
+    }
+
+    public ListNode mergeTwoLists(ListNode l1,ListNode l2){
+        return null;
+    }
+}
