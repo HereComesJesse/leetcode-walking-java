ff066a2e0bcbde37f2332e9167db30c32d0ad2a2
jdzhang <673235694@qq.com>
2019-08-16 23:56:41 +0800

    initial removeDuplicates

--- src/main/java/Array/RemoveDuplicates.java
+++ src/main/java/Array/RemoveDuplicates.java
@@ -22,7 +22,13 @@ public class RemoveDuplicates {
             return 0;
         }
         int i=0;
-        return 0;
+        for (int j=1;j<nums.length;j++){
+            if(nums[i]!=nums[j]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
     }
 
     public static void main(String[] args) {
