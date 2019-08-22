c75a777afb15a0444fdbe6b7e67c3ce6f99280a2
jdzhang <673235694@qq.com>
2019-08-22 21:11:53 +0800

    add LengthOfLastWord

---
+++ src/main/java/Array/LengthOfLastWord.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2019 tuhu.cn All right reserved. This software is the
+ * confidential and proprietary information of tuhu.cn ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Tuhu.cn
+ */
+package Array;
+
+import org.apache.commons.lang3.StringUtils;
+/**
+ * @author zhangjundong
+ * @date 2019/8/2220:33
+ */
+public class LengthOfLastWord {
+    /**
+     * Description:
+     * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+     *
+     * If the last word does not exist, return 0.
+     *
+     * Note: A word is defined as a character sequence consists of non-space characters only.
+     * @param s
+     * @return
+     */
+    public int LengthOfLastWord(String s){
+        return s.trim().length()-s.trim().lastIndexOf(" ")-1;
+    }
+
+    public static void main(String[] args) {
+        String input="Hello world";
+        int output= new LengthOfLastWord().LengthOfLastWord(input);
+        System.out.println(output);
+    }
+}
