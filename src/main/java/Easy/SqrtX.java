144c6930de6f2c5186db5ca212f4c4bb953b8ad3
jdzhang <673235694@qq.com>
2019-08-25 23:54:01 +0800

    add SqrtX

---
+++ src/main/java/Easy/SqrtX.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2019 tuhu.cn All right reserved. This software is the
+ * confidential and proprietary information of tuhu.cn ("Confidential
+ * Information"). You shall not disclose such Confidential Information and shall
+ * use it only in accordance with the terms of the license agreement you entered
+ * into with Tuhu.cn
+ */
+package Easy;
+
+/**
+ * @author zhangjundong
+ * @date 2019/8/2523:38
+ */
+public class SqrtX {
+    public int mySqrt(int x){
+        
+    }
+
+    public static void main(String[] args) {
+        int x=2147483647;
+        int output= new SqrtX().mySqrt(x);
+        System.out.println(output);
+    }
+}
